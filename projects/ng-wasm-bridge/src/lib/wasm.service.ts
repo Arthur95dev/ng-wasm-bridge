6d6492629ffb52b3b327ea43440713c4a738dfee
Artur Efremov <artur_efremov_95@bk.ru>
2025-11-24 02:06:46 +0300

    update service

--- projects/ng-wasm-bridge/src/lib/wasm.service.ts
+++ projects/ng-wasm-bridge/src/lib/wasm.service.ts
@@ -4,31 +4,40 @@ import { EmscriptenModule, TypedModule } from '../types';
 @Injectable({ providedIn: 'root' })
 export class WasmService {
 
-  private modules: Record<string, EmscriptenModule> = {};
+  private modulesPromises: Map<string, Promise<EmscriptenModule>> = new Map();
+  private modules: Map<string, EmscriptenModule> = new Map();
 
-  /**
-   * Загружает модуль
-   * @param path Путь к файлу модуля
-   */
   async loadWasmModule<T>(path: string): Promise<TypedModule<T>> {
-    if (this.modules[path]) {
-      return this.modules[path] as TypedModule<T>;
+    let module = this.modules.get(path);
+
+    if (module) {
+      return module as TypedModule<T>;
+    } else if (this.modulesPromises.has(path)) {
+      return this.modulesPromises.get(path) as Promise<TypedModule<T>>;
     }
 
+    const promiseModule = this.load(path);
+    this.modulesPromises.set(path, promiseModule);
+
+    return promiseModule as Promise<TypedModule<T>>;
+  }
+
+  private async load<T>(path: string): Promise<TypedModule<T>> {
     const factoryModule = await import(/* @vite-ignore */ path);
     const factory = factoryModule.default;
 
     const module = await factory();
-    this.modules[path] = module;
-
+    this.modules.set(path, module);
+    this.modulesPromises.delete(path);
     return module;
   }
+
 
   /**
    * Получает модуль по имени
    * @param path Путь к файлу модуля
    */
   getModule<T>(path: string): TypedModule<T> | null {
-    return (this.modules[path] as TypedModule<T>) || null;
+    return (this.modules.get(path) as TypedModule<T>) || null;
   }
 }
